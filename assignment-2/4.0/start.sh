1b11ed848ae5a0b79d94077868a57bdbfb30c3f3
someshsingh <someshs91@gmail.com>
2025-08-03 09:00:09 +0530

    fixed start.sh

--- assignment-2/4.0/start.sh
+++ assignment-2/4.0/start.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-sudo chown -R $(whoami) ./backend/docker
+sudo chown -R 100:100 ./backend/docker
 sudo chmod -R 755 ./backend/docker
 docker compose build
 docker compose up
-#cd frontend
-#npm install
-#npm run dev
+cd frontend
+npm install
+npm run dev
